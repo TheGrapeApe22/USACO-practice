54a2d738bb66146435f879fd4c94dab5ae45bd57
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-01-08 21:27:14 -0800

    lemonade line

---
+++ past/cses/sorting-searching/reading_books.cpp
@@ -0,0 +1,57 @@
+// https://cses.fi/problemset/task/1631/
+#include <bits/stdc++.h>
+#define vec vector
+#define ln "\n"
+#define _ << " " <<
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+namespace {
+    template<typename it> void print(it, it);
+    template<typename T> void print(T& v) {print(all(v));}
+    template<typename T, typename U> void print(pair<T, U> p) { cout << "("<<p.first<<", "<<p.second<<")"; }
+    template<typename T, typename U>ostream& operator<<(ostream& out, pair<T, U> p) { print(p); return out; }
+    template<typename T> ostream& operator<<(ostream& out, const vector<T>& v) { print(v); return out; }
+    template<typename T> ostream& operator<<(ostream& out, const set<T>& v) { print(v); return out; }
+    template<typename T> ostream& operator<<(ostream& out, const multiset<T>& v) { print(v); return out; }
+    template<typename T, typename U> ostream& operator<<(ostream& out, const map<T, U>& v) { print(v); return out; }
+    template<typename it> void print(it begin, it end) {
+        cout << "[";
+        if (begin != end) {
+            auto last = --end;
+            for (; begin != last; begin++)
+                cout << *begin << ", ";
+            cout << *last;
+        }
+        cout << "]";
+    }
+    template<typename T> void println(T v) {print(v); cout << "\n";}
+}
+
+/*
+ans = sum(all books)
+if 2*largest>sum
+    ans = 2*largest
+*/
+
+int main() {
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    
+    ll n;
+    cin >> n;
+
+    ll sum = 0;
+    ll largest = 0;
+    for (ll i = 0; i < n; i++) {
+        ll k; cin >> k;
+        sum += k;
+        largest = max(largest, k);
+    }
+    ll ans;
+    if (2*largest > sum)
+        ans = 2*largest;
+    else
+        ans = sum;
+    cout << ans << ln;
+}

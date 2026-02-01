69402789d20216ede9f0e5895738f92c42524b0c
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-02-01 10:45:29 -0800

    cf boredom

---
+++ solutions/cf/practice/boredom.cpp
@@ -0,0 +1,55 @@
+// https://codeforces.com/problemset/problem/455/A
+// looked at solution
+#include <bits/stdc++.h>
+#define vec vector
+#define ln "\n"
+#define _ << " " <<
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+namespace {
+    template<typename it> void print(it, it);
+    template<typename T> void print(T& v) {print(all(v));}
+    template<typename T, typename U> void print(pair<T, U> p) { cout << "("<<p.first<<", "<<p.second<<")"; }
+    template<typename T, typename U>ostream& operator<<(ostream& out, pair<T, U> p) { print(p); return out; }
+    template<typename T> ostream& operator<<(ostream& out, const vector<T>& v) { print(v); return out; }
+    template<typename T> ostream& operator<<(ostream& out, const set<T>& v) { print(v); return out; }
+    template<typename T> ostream& operator<<(ostream& out, const multiset<T>& v) { print(v); return out; }
+    template<typename T, typename U> ostream& operator<<(ostream& out, const map<T, U>& v) { print(v); return out; }
+    template<typename it> void print(it begin, it end) {
+        cout << "[";
+        if (begin != end) {
+            auto last = --end;
+            for (; begin != last; begin++)
+                cout << *begin << ", ";
+            cout << *last;
+        }
+        cout << "]";
+    }
+    template<typename T> void println(T v) {print(v); cout << "\n";}
+}
+
+int main() {
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+
+    vec<ll> freq (1e5+1);
+    for (int i = 0; i < n; i++) {
+        int k; cin >> k;
+        freq[k]++;
+    }
+
+    // dp[i] = max score using numbers from 0 to i
+    vec<ll> dp (1e5+1);
+    dp[0] = 0;
+    dp[1] = freq[1];
+
+    for (ll x = 2; x <= 1e5; x++) {
+        dp[x] = max(dp[x-1], dp[x-2] + freq[x]*x);
+    }
+
+    cout << dp[1e5] << ln;
+}

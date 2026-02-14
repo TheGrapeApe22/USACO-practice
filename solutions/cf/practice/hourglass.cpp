965823d2ba7e95d4405c900bbef0dcd0da724c83
thegrapeape22 <rohan.nadkarni123@gmail.com>
2026-02-14 11:16:41 -0800

    cf hourglass

--- solutions/cf/practice/hourglass.cpp
+++ solutions/cf/practice/hourglass.cpp
@@ -1,3 +1,4 @@
+// https://codeforces.com/contest/2184/problem/B
 #include <bits/stdc++.h>
 #define vec vector
 #define ln "\n"
@@ -8,8 +9,29 @@ template<typename T> ostream& operator<<(ostream& out, const vector<T>& v) {
     cout << "["; for (size_t i = 0; i+1 < v.size(); i++) cout << v[i] << ", "; cout << *v.rbegin() << "]"; return out;
 }
 
+/*
+time 0: s (maximum)
+time k: s-k, or 0 (minimum)
+
+m %= 2*k
+if m<k:
+    return max(0, s-m)
+else:
+    return max(0, s - (max(0, s-k) + m-k))
+*/
+
+int solve() {
+    int s, k, m;
+    cin >> s >> k >> m;
+    m %= 2*k;
+    if (m<k)
+        return max(0, s-m);
+    else
+        return max(0, s - (max(0, s-k) + m-k));
+}
+
 int main() {
-    // freopen("file.in", "r", stdin); freopen("file.out", "w", stdout);
     cin.tie(nullptr); ios::sync_with_stdio(false);
-    
+    int t; cin >> t;
+    for (int i = 0; i < t; i++) cout << solve() << ln;
 }
